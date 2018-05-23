8fc276f2fc463193c1b0f1411030d382ebc765fc
Samuel Gluss <sam@ujet.co>
2018-05-23 15:09:44 -0700

    fixed ping command

--- advanced_functionality/scikit_bring_your_own/container/decision_trees/predictor.py
+++ advanced_functionality/scikit_bring_your_own/container/decision_trees/predictor.py
@@ -94,7 +94,8 @@ app = flask.Flask(__name__)
 def ping():
     """Determine if the container is working and healthy. In this sample container, we declare
     it healthy if we can load the model successfully."""
-    health = ScoringService.get_model() is not None  # You can insert a health check here
+    global scoringService
+    health = scoringService.get_model() is not None  # You can insert a health check here
 
     status = 200 if health else 404
     return flask.Response(response='\n', status=status, mimetype='application/json')
